dbe79c62f19f8fe107967600372d3aea467f69b9
Levi Soares <levi-castro@outlook.com>
2024-06-10 19:44:19 -0300

    Initial Commit

---
+++ Aprendendo_IMC/Aprendendo_IMCApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aprendendo_IMCApp.swift
+//  Aprendendo_IMC
+//
+//  Created by levi soares on 10/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Aprendendo_IMCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
